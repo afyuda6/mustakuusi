40da9c598d966863cad2318c1d66c68f3209d11e
Yuda <yuda_p411@apps.ipb.ac.id>
2025-12-27 17:08:23 +0700

    27/12/2025

--- src/pages/PrivacyPolicyPage.tsx
+++ src/pages/PrivacyPolicyPage.tsx
@@ -1,10 +1,17 @@
 import {useParams} from "react-router-dom";
+import projects from "../data/projects.json";
 import {Privacy} from "../components/Privacy/Privacy";
 import {Contact} from "../components/Contact/Contact.tsx";
 import {Navbar} from "../components/Navbar/Navbar.tsx";
 
 export const PrivacyPolicyPage = () => {
     const {id} = useParams();
+    const project = projects.find((p) => p.id === id);
+
+    if (!project) {
+        return <div></div>;
+    }
+
     return (
         <div>
             <Navbar/>
